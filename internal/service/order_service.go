0f654c01e3ab46012ccbf0cb62a79aab2eb3898c
Erik <aktaeverikwork@gmail.com>
2025-08-24 14:45:35 +0500

    interfaces on the usage are written

--- internal/service/order_service.go
+++ internal/service/order_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "wb-tech-1task/internal/models"
+import (
+	"context"
+	"wb-tech-1task/internal/models"
+)
 
 type OrderCache interface {
 	Set(order *models.Order) error
@@ -8,6 +11,28 @@ type OrderCache interface {
 	GetAll() (map[string]*models.Order, error)
 	Delete(orderUID string)
 	Count() int
+	DBBackup(orders []*models.Order) error
 }
 
-cs
+type OrderRepository interface {
+	GetOrder(ctx context.Context, orderUID string) (*models.Order, error)
+	SaveOrder(ctx context.Context, order *models.Order) error
+	GetAllOrders(ctx context.Context) ([]*models.Order, error)
+	Close() error
+}
+
+type OrderService struct {
+	cache OrderCache
+	repo  OrderRepository
+}
+
+func NewOrderService(cache OrderCache, repo OrderRepository) *OrderService {
+	return &OrderService{
+		cache,
+		repo,
+	}
+}
+
+func (s *OrderService) GetOrder(ctx context.Context, orderUID string) (*models.Order, error) {
+	order, exists, err :=
+}
